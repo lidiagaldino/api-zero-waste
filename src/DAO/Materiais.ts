d96fb86bbf62b777e7edb054829a8ad54266b4de
lidiagaldino <lidiagabrielly14@gmail.com>
2023-02-16 10:39:51 -0300

    tratamento de erros durante cadastro de usuário

--- src/DAO/Materiais.ts
+++ src/DAO/Materiais.ts
@@ -7,7 +7,23 @@ class Materiais {
 
         try {
 
-            const sql = `insert into tbl_materiais_catador(id_catador, id_material) values(${id_catador}, ${id_material})`
+            const sql = `insert into tbl_materiais_catador(id_catador, id_materiais) values(${id_catador}, ${id_material})`
+            const result = await prisma.$executeRawUnsafe(sql)
+            console.log(result)
+
+            return (!!result)
+
+        } catch (error) {
+            return false
+        }
+    }
+
+    public async deleteMateriaisCatador(id: number): Promise<boolean> {
+
+        try {
+
+            const sql = `delete from tbl_materiais_catador where id_catador = ${id}`
+
             const result = await prisma.$executeRawUnsafe(sql)
 
             return (!!result)
