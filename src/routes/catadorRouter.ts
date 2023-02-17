e5da88c59063491fcdf20313442f4b86a29522ea
lidiagaldino <lidiagabrielly14@gmail.com>
2023-02-17 09:28:22 -0300

    configurando rotas

--- src/routes/catadorRouter.ts
+++ src/routes/catadorRouter.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
 import CatadorController from '../controllers/CatadorController'
+import { catadorBodyValidation } from '../schemas/catadorSchema'
+import { verify } from '../middleware/verify'
+import { validation } from '../middleware/validation'
 
 const routes = Router()
 
 routes.get('/', CatadorController.index)
+routes.post('/', validation({ body: catadorBodyValidation }), verify, CatadorController.store)
 
 export default routes
--- src/routes/geradorRouter.ts
+++ src/routes/geradorRouter.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
 import GeradorController from '../controllers/GeradorController'
+import { geradorBodyValidation } from '../schemas/geradorSchema'
+import { verify } from '../middleware/verify'
+import { validation } from '../middleware/validation'
 
 const routes = Router()
 
 routes.get('/', GeradorController.index)
+routes.post('/', validation({ body: geradorBodyValidation }), verify, GeradorController.store)
 
 export default routes
