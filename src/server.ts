2a47b0d35ef3402954909034eda436efe6e9af83
lidiagaldino <lidiagabrielly14@gmail.com>
2023-03-08 10:31:07 -0300

    deploy no azure

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,66 @@
 import app from './app'
+import http from 'http'
+import debug from 'debug'
 
-app.listen(3000, () => console.log('Server rodando'))
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+server.on('error', onError);
+server.on('listening', onListening);
+
+console.log('ApI rodando na porta: ' + port);
+
+function normalizePort(val: string) {
+
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+
+        return val;
+    }
+
+    if (port >= 0) {
+
+        return port;
+    }
+
+    return false;
+}
+
+function onError(error: { syscall: string; code: any; }) {
+
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    switch (error.code) {
+
+        case 'EACESS':
+            console.error(bind + 'requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + 'is alredy in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
