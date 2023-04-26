c02a58ebde011748be5b3080d280ef37a3c043aa
lidiagaldino <lidiagabrielly14@gmail.com>
2023-04-26 07:39:56 -0300

    ajuste

--- src/middleware/verifyUser.ts
+++ src/middleware/verifyUser.ts
@@ -1,10 +1,17 @@
-import { NextFunction, Request, Response } from "express"
-import { StatusCodes } from "http-status-codes"
-import Endereco from "../DAO/Endereco"
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import Endereco from "../DAO/Endereco";
 
-export const verifyUser = async (req: Request, res: Response, next: NextFunction) => {
+export const verifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.id_usuario == req.params.id_usuario) return next();
 
-    if (req.id_usuario == req.params.id_usuario) next()
-
-    return res.status(StatusCodes.BAD_REQUEST).json({message: 'Token não pertence ao mesmo usuário informado nos parametros'})
-}
+  return res
+    .status(StatusCodes.BAD_REQUEST)
+    .json({
+      message: "Token não pertence ao mesmo usuário informado nos parametros",
+    });
+};
